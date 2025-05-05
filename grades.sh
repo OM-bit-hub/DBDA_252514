ce70e5947f20bc295f881200b35ff5ee56b97fec
Om Dhengle <84015836+OM-bit-hub@users.noreply.github.com>
2025-05-05 12:57:51 +0530

    Update grades.sh

--- grades.sh
+++ grades.sh
@@ -2,11 +2,14 @@ read -p "Enter marks of Linux" linux
 read -p "Enter marks of Python" python
 read -p "Enter marks of DBMS" dbms
 avg=(((linux+python+dbms)/3))
-if [[ $((avg>=70)) ]]; then
+if ((avg>=70)) ; then
 echo "A+"
-elif [[ $((avg>=70)) ]]; then
+elif (( avg >= 60 && avg < 70 )); then
+echo "A"
+elif (( marks >= 50 && marks < 60 )); then
 echo "B"
-elif [[ $((avg>=70)) ]]; then
+
+elif (( marks > 40 && marks <= 50 )); then
 echo "C"
 else
 echo "F"
